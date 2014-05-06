139882998c7d9cd004c5b65d3070333cee8830f6
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-06 15:04:16 -0700

    failing deactivate test

--- spec/feature_release_spec.rb
+++ spec/feature_release_spec.rb
@@ -44,6 +44,16 @@ describe FeatureRelease do
       @fr.active?(:test_feature, stub(:new? => true)).should be_true
     end
 
+    it "deactivates a feature for a defined group" do
+      @fr = FeatureRelease.new
+      @fr.define_group(:testers) do |user|
+        user.new?
+      end
+      @fr.activate_feature(:test_feature, :testers)
+      @fr.deactivate_feature(:test_feature, :testers)
+      @fr.active?(:test_feature, stub(:new? => true)).should be_false
+    end
+
   end
 
 end

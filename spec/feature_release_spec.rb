0265a3998e50ea43b3f69fe01f7d64f94724bef9
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-06 07:37:16 -0700

    refactor feature_release specs

--- spec/feature_release_spec.rb
+++ spec/feature_release_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 describe FeatureRelease do
 
-  it "initializes with a default group proc" do
-    fr = FeatureRelease.new
-    fr.groups[:all].should be_kind_of(Proc)
-  end	
+  context '#new' do
 
-  it "initializes with a features variable" do
-  	fr = FeatureRelease.new
-  	fr.features.should_not be_nil
+  	before(:each) do
+  	  @fr = FeatureRelease.new
+  	end
+
+  	it "initializes with a default group proc" do
+  	  @fr.groups[:all].should be_kind_of(Proc)
+  	end
+
+  	it "initializes with a features variable" do
+  	  @fr.features.should_not be_nil
+  	end
+
   end
 
 end

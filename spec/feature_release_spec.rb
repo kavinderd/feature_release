cfee518973e5ad66d96a40f0893f433e1d902eb3
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-06 07:22:02 -0700

    failing FeatureRelease init test

--- spec/feature_release_spec.rb
+++ spec/feature_release_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe FeatureRelease do
-	
+
+  it "initializes with a default group proc" do
+    fr = FeatureRelease.new
+    fr.groups[:all].should be_kind_of(Proc)
+  end	
+
 end

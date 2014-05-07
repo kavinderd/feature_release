1a9c78e585a6a477d1a9abfa35ef61eaa840dbb7
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-06 17:29:10 -0700

    failing feature init spec

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe FeatureRelease::Feature do
+
+
+  it "initializes with a name" do
+  	expect{f = FeatureRelease::Feature.new(:test_feature)}.to_not raise_error
+  end
 	
 end

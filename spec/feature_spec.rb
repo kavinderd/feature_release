5b04d1d2b80527b9e68594b3dc6c931504d6cbbd
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-06 17:34:45 -0700

    failing feature groups spec

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -6,5 +6,10 @@ describe FeatureRelease::Feature do
   it "initializes with a name" do
   	expect{f = FeatureRelease::Feature.new(:test_feature)}.to_not raise_error
   end
+
+  it "sets a default array to groups" do
+  	f = FeatureRelease::Feature.new(:test_feature)
+  	f.groups.should eq([])
+  end
 	
 end

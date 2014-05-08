f7ca3cb3d783b9082dc5f7776f96bdf4dce9bbf8
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-08 14:57:56 -0700

    failing Storage initialize spec

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe FeatureRelease::Storage do
-  
+
+  before(:each) do
+  	@redis = MockRedis.new
+  end
+
+  it "should initialize with a store" do
+  	expect{FeatureRelease::Storage.new(store: nil)}.to raise_error
+  	expect{FeatureRelease::Storage.new(store: @redis)}.to_not raise_error
+  end
+
 end

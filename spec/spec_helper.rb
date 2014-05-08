cc43a15d9d1c95c6f4978396d870bf5dc6496da2
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-05-08 14:52:35 -0700

    require mock redis in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'feature_release.rb'
+require 'mock_redis'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
